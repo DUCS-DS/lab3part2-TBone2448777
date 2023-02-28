11b806b2473d50083b8fff03fab4bbd2540ebdd0
TBone2448777 <taylorsloan3430@gmail.com>
2023-02-28 10:35:52 -0600

    Completed tortoise hare cycle search

--- cyclic.py
+++ cyclic.py
@@ -1,14 +1,35 @@
 from llist import *
+from gencyclic import lst
 
 def llprint(lst, num):
     """print the first num terms of linked list lst"""
-    pass
+    i = 0
+    if lst.head:
+        node = lst.head
+        while node.next and i <= num:
+            print(node.val, end=", ")
+            node = node.next
+            i += 1
+        print(node.val)
 
 def find_cycle(lst):
     """return the start index and length of the cycle"""
-    pass
-
+    tortoise = lst.head
+    hare = lst.head.next.next
+    i = 0
+    while tortoise is not hare:
+        tortoise = tortoise.next
+        hare = hare.next.next
+        i += 1
+    saved = tortoise
+    tortoise = tortoise.next
+    j = 0
+    while saved is not tortoise:
+        tortoise = tortoise.next
+        j += 1
+    return i, j
 
 if __name__ == "__main__":
-
-    pass
+    start, length = find_cycle(lst)
+    print("The cycle begins at node " + str(start) + ".")
+    print("The cycle is " + str(length) + " items long.")
